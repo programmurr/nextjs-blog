56171f54120bb4b6af99a9f8323324203eb32f5e
Conor Murray <64764695+programmurr@users.noreply.github.com>
2023-10-28 00:36:09 +0100

    Set up e2e and component testing with Cypress

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,7 +33,7 @@ export default function Home({
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>ESLint has been set up!</p>
+        <p>Cypress testing set up!</p>
         <p>
           (This is a sample website - you&apos;ll be building a site like this
           on <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
